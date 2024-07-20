5d2d8ae0f25b919eddfc989efed1689d9af58e6d
nespinoza2000 <nespinozasegovia@gmail.com>
2024-07-20 11:34:43 -0400

    Componente de Loading añadido

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-import MapComponent from './components/Map.jsx'
+import { useState, useEffect } from 'react';
+import MapComponent from './components/Map.jsx';
+import { Loading } from './components/Loading.jsx';
 
 const App = () => {
-  let [places, setPlaces] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      setPlaces([
+        { key: '1', location: { lat: -27.33056, lng: -55.86667 } }
+      ]);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div className="w-screen h-screen m-0 p-3 bg-slate-500">
-        <MapComponent locations={places}/>
-      </div>
-    </>
+    <div className="w-screen h-screen m-0 p-0 bg-slate-500">
+      {loading ? (
+        <Loading />
+      ) : (
+        <MapComponent locations={places} />
+      )}
+    </div>
   );
 };
 
