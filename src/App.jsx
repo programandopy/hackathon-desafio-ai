336f9957c012580706af0d91ea8c37ba46a9cdd6
marcosferr <ferreiramarcosadrian@gmail.com>
2024-07-20 11:49:31 -0400

    Open AI Service to get Places. Limited and Not Limited

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import MapComponent from './components/Map.jsx'
-
+import { useEffect, useState } from "react";
+import MapComponent from "./components/Map.jsx";
+import openaiService from "./services/openai.js";
 const App = () => {
   let [places, setPlaces] = useState([]);
 
+  const getPlaces = async (prompt) => {
+    places = await openaiService.getPlacesAI(prompt);
+    console.log(places);
+    setPlaces(places);
+  };
+
+  useEffect(() => {
+    getPlaces("Muestrame los 10 mejores lugares de tecnología en Encarnación");
+  }, []);
   return (
     <>
       <div className="w-screen h-screen m-0 p-3 bg-slate-500">
-        <MapComponent locations={places}/>
+        <MapComponent locations={places} />
       </div>
     </>
   );
